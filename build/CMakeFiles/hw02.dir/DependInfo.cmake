da1ec44fb6fa5b0d1f9035d692c19c5ca56145f4
Davide Scrimieri <dscrimie@dscrimie-thinkpadt14gen5.rmtit.csb>
2025-10-15 11:53:25 +0200

    .

---
+++ build/CMakeFiles/hw02.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/dscrimie/Documents/University/Cybersecurity/HOMEWORK/hw02/generate_files.c" "CMakeFiles/hw02.dir/generate_files.c.o" "gcc" "CMakeFiles/hw02.dir/generate_files.c.o.d"
+  "" "hw02" "gcc" "CMakeFiles/hw02.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/hw02.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw02.dir/link.d"
+  "CMakeFiles/hw02.dir/generate_files.c.o"
+  "CMakeFiles/hw02.dir/generate_files.c.o.d"
+  "hw02"
+  "hw02.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hw02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
